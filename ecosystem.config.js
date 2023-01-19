9a2015a2bd3541aba6565d1ccda558516e1ea542
Ivan <prikot@live.ru>
2023-01-19 16:09:10 +0700

    port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       cron_restart: '0 0 * * *',
       //host: '188.225.44.134',
-      port: 3025,
+      port: 3027,
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
