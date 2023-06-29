ec24e73c23e113281e0691431b7fb46dcfe4ddde
Ivan <prikot@live.ru>
2023-06-29 18:34:15 +0700

    Контакты

--- components/VContacts/index.vue
+++ components/VContacts/index.vue
@@ -1,45 +1,52 @@
 <template>
     <div class="contacts">
-        <div
-            class="
-                d-sm-flex d-block
-                mb-9 mb-md-12
-                align-center
-                justify-space-between
-            "
-        >
+        <div class="d-sm-flex d-block mb-9 mb-md-12 align-center justify-space-between">
             <h2 class="text-uppercase">Контакты</h2>
             <div v-if="showLink">
                 <nuxt-link
                     class="font-weight-bold underline-href contacts-link"
                     to="/contacts/"
-                    >Перейти на страницу контактов</nuxt-link
-                >
+                >Перейти на страницу контактов</nuxt-link>
             </div>
         </div>
 
         <client-only>
             <div class="contacts__main">
                 <v-card class="contacts__card shadow-card">
-                    <p class="card-title font-weight-bold mb-4">
-                        Выберите филиал
-                    </p>
+                    <p class="card-title font-weight-bold mb-4">Выберите филиал</p>
                     <template v-if="contacts && contacts.length > 0">
-                        <div class="contacts__card-i">
-                            <div
-                                class="d-none d-md-block contacts__city"
-                                @click="setCurrentCity(item)"
-                                :class="
+                        <div class="contacts__card-i d-none d-md-block">
+                            <div v-for="item in contacts" :key="item.id" class="contacts__city">
+                                <span
+                                    @click="setCurrentCity(item)"
+                                    :class="
                                     currentColoredCity == item.id
                                         ? 'contacts__city-colored'
                                         : ''
                                 "
-                                v-for="item in contacts"
-                                :key="item.id"
-                            >
-                                {{ item.name }}
+                                >{{ item.name }}</span>
+                                <nuxt-link
+                                    :to="{name: 'contacts-slug', params: { slug: item.slug }}"
+                                    class="contacts__city-link"
+                                    target="_blank"
+                                    title="Открыть контакт в новой вкладке"
+                                >
+                                    <svg
+                                        viewBox="0 0 32 32"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M22 3h7v7M27.5 4.5L20 12M17 5H8a3 3 0 00-3 3v16a3 3 0 003 3h16a3 3 0 003-3v-9"
+                                        />
+                                    </svg>
+                                </nuxt-link>
                             </div>
-                        </div> 
+                        </div>
                         <v-select
                             class="d-block d-md-none contacts__city-input"
                             v-on:change="setCurrentCity()"
@@ -54,8 +61,7 @@
                             validate-on-blur
                             persistent-hint
                             outlined
-                        >
-                        </v-select>
+                        ></v-select>
                     </template>
                 </v-card>
 
@@ -65,41 +71,29 @@
                         :coords="currentCity.map.coords"
                     ></v-contacts-map>
                     <div class="map-disable-wrapper" v-else></div>
-                    <v-card
-                        class="contacts__main-info shadow-card"
-                        v-if="currentCity"
-                    >
-                        <div
-                            class="contacts__single pr-0 pr-sm-8 pr-lg-0"
-                            v-if="currentCity.phone"
-                        >
+                    <v-card class="contacts__main-info shadow-card" v-if="currentCity">
+                        <div class="contacts__single pr-0 pr-sm-8 pr-lg-0" v-if="currentCity.phone">
                             <div class="contacts_img-wrapper mb-4 mb-md-8">
                                 <img src="/img/icons/phone.svg" alt="phone" />
                             </div>
-                            <p class="card-title font-weight-bold mb-2">
-                                Телефон
-                            </p>
+                            <p class="card-title font-weight-bold mb-2">Телефон</p>
                             <div>
                                 <a
                                     class="card-small-text"
                                     :href="`tel:${currentCity.phone.value}`"
-                                    >{{ currentCity.phone.name }}</a
-                                >
+                                >{{ currentCity.phone.name }}</a>
                             </div>
                         </div>
                         <div class="contacts__single" v-if="currentCity.email">
                             <div class="contacts_img-wrapper mb-4 mb-md-8">
                                 <img src="/img/icons/mail.svg" alt="mail" />
                             </div>
-                            <p class="card-title font-weight-bold mb-2">
-                                E-mail
-                            </p>
+                            <p class="card-title font-weight-bold mb-2">E-mail</p>
                             <div>
                                 <a
                                     class="card-small-text"
                                     :href="`mailto:${currentCity.email.value}`"
-                                    >{{ currentCity.email.name }}</a
-                                >
+                                >{{ currentCity.email.name }}</a>
                             </div>
                         </div>
                         <div
@@ -109,28 +103,16 @@
                             <div class="contacts_img-wrapper mb-4 mb-md-8">
                                 <img src="/img/icons/marker.svg" alt="marker" />
                             </div>
-                            <p class="card-title font-weight-bold mb-2">
-                                Адрес
-                            </p>
+                            <p class="card-title font-weight-bold mb-2">Адрес</p>
                             <div>
-                                <p class="card-small-text">
-                                    {{ currentCity.address }}
-                                </p>
+                                <p class="card-small-text">{{ currentCity.address }}</p>
                             </div>
                         </div>
-                        <div
-                            class="contacts__single"
-                            v-if="currentCity.operating_mode.length > 0"
-                        >
+                        <div class="contacts__single" v-if="currentCity.operating_mode.length > 0">
                             <div class="contacts_img-wrapper mb-4 mb-md-8">
-                                <img
-                                    src="/img/icons/graphic.svg"
-                                    alt="graphic"
-                                />
+                                <img src="/img/icons/graphic.svg" alt="graphic" />
                             </div>
-                            <p class="card-title font-weight-bold mb-2">
-                                График работы
-                            </p>
+                            <p class="card-title font-weight-bold mb-2">График работы</p>
                             <div>
                                 <div
                                     class="card-small-text"
@@ -139,8 +121,7 @@
                                     ) in currentCity.operating_mode"
                                     :key="index"
                                 >
-                                    <span>{{ item.name }}</span
-                                    >:
+                                    <span>{{ item.name }}</span>:
                                     <span>{{ item.value }}</span>
                                 </div>
                             </div>
@@ -164,8 +145,8 @@ export default {
     props: {
         showLink: {
             type: Boolean,
-            default: true
-        }
+            default: true,
+        },
     },
 
     data() {
@@ -179,7 +160,7 @@ export default {
     computed: {
         ...mapGetters({
             contacts: "getContacts",
-            dynamicContact: "getDynamicContact"
+            dynamicContact: "getDynamicContact",
         }),
     },
 
@@ -187,19 +168,18 @@ export default {
         contacts: {
             handler(newCity, oldCity) {
                 if (!oldCity && newCity?.length > 0) {
-                    this.setCurrentCity(this.dynamicContact);
+                    this.setCurrentCity(this.dynamicContact || newCity[0]);
                 }
             },
-            immediate: true
+            immediate: true,
         },
     },
+
     methods: {
         setCurrentCity(objCity = false) {
             if (objCity) {
                 this.currentCity = { ...objCity };
                 this.currentColoredCity = objCity.id;
-            } else {
-                this.currentColoredCity = this.currentCity.id;
             }
         },
     },
@@ -223,7 +203,7 @@ export default {
     }
 
     &::-webkit-scrollbar {
-        width: 8px;        
+        width: 8px;
     }
 
     &::-webkit-scrollbar-track {
@@ -238,7 +218,6 @@ export default {
     }
 }
 .contacts {
-
     .contacts__info {
         .v-card {
             padding: 30px;
@@ -263,14 +242,37 @@ export default {
             flex: auto;
         }
     }
-    
+
     .contacts__city {
         padding: 4px 0;
         cursor: pointer;
-        transition: 0.5s all;
+        display: flex;
+        align-items: center;
+        gap: 8px;
 
         & + .contacts__city {
             margin-top: 8px;
+        }
+
+        span {
+            transition: 0.5s all;
+
+            &:hover,
+            &.contacts__city-colored {
+                color: $hover-color;
+            }
+        }
+    }
+    .contacts__city-link {
+        display: inline-block;
+        color: rgba(0, 0, 0, 0.7);
+        line-height: 1;
+        width: 26px;
+        height: 26px;
+
+        svg {
+            width: 100%;
+            height: 100%;
         }
     }
     .contacts__main-info {
@@ -282,7 +284,7 @@ export default {
         row-gap: 30px;
 
         a {
-            color: #231F20;
+            color: #231f20;
         }
     }
     .contacts__single-adress {
---
+++ pages/contacts/_slug.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="content-padding">
+        <v-container>
+            <h1 class="text-uppercase mb-md-14 mb-9">{{ title }}</h1>
+
+            <div v-if="contact.content" class="block-padding">
+                <div v-html="contact.content"></div>
+            </div>
+
+            <v-row v-if="images.length" class="block-padding">
+                <v-col v-for="(image, i) in images" :key="i" cols="6">
+                    <v-img :src="image.url" :alt="image.alt" :title="image.title"></v-img>
+                </v-col>
+            </v-row>
+
+            <v-contacts :show-link="false"></v-contacts>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import VContacts from "~/components/VContacts/index.vue";
+
+export default {
+    components: {
+        VContacts,
+    },
+
+    head() {
+        const title = this.contact?.seo.title || this.title;
+
+        return {
+            title: `${title} - Официальный дилер техники`,
+            meta: [
+                { charset: "utf-8" },
+                {
+                    hid: "description",
+                    name: "description",
+                    content: this.contact?.seo?.description ?? "",
+                },
+                { hid: "keywords", name: "keywords", content: this.contact?.seo?.keywords ?? "" },
+            ],
+        };
+    },
+
+    computed: {
+        title() {
+            return this.contact.title || this.contact.name;
+        },
+
+        images() {
+            return this.contact?.images || [];
+        }
+    },
+
+    async asyncData({ store, params, error }) {
+        const { slug } = params;
+        
+        const contact = await store
+            .dispatch("loadContact", slug)
+            .then((r) => r.data.data)
+            .catch(() => {
+                error({ statusCode: 404, message: "Страница не найдена" });
+            });
+        
+        store.commit("onVisabilityHeader");
+        store.commit("setBreadCrumbs", [
+            {
+                text: "Официальный дилер XCMG",
+                disabled: false,
+                href: "/",
+                position: 1,
+            },
+            {
+                text: "Контакты",
+                disabled: false,
+                href: "/contacts",
+                position: 2,
+            },
+            {
+                text: contact.title || contact.name,
+                disabled: true,
+                href: "",
+                position: 3,
+            },
+        ]);
+
+        return {
+            contact,
+        };
+    },
+};
+</script>
